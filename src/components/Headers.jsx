e2fab041b5315ffcfa00b2af28ecee3d26ca5f8a
Riya Rastogi <riya02rastogi@gmail.com>
2025-12-27 16:08:22 +0530

    resizing the logo img

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -4,8 +4,8 @@ import {LOGO_URL} from "../constant";
 const Header = () => {
 
   return (
-    <div className="flex justify-between sticky top-0 bg-[#fcfcfc]">
-      <img className="w-[109px] h-[86px]" src={LOGO_URL} alt="PokemonLogo" />
+    <div className="flex items-center justify-between sticky top-0 z-50 bg-[#fcfcfc] p-2">
+      <img className="w-[109px] h-auto" style={{ width: "109px" }} src={LOGO_URL} alt="PokemonLogo" />
     </div>
   );
 };
